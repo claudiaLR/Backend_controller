4fb0c9cc6f7ffca97ce80d8fcbb09dfb0ba986ae
claudialemus <cl897665@gmail.com.example>
2019-12-09 10:02:36 -0600

    proyecto js y ajax con una anotacion

---
+++ repositorios/IEspecialidad.java
@@ -0,0 +1,10 @@
+package com.example.crudConAjax.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.crudConAjax.entidades.Especialidad;
+
+public interface IEspecialidad  extends CrudRepository<Especialidad, Integer>{
+
+	
+}
